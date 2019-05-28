efa0290fd725984a9881f4f2f36c86d686b31086
XunzhaoYu <yuxunzhao@gmail.com>
2019-05-28 23:54:04 +0100

    exercise for day 76

--- easy/661.Image Smoother.py
+++ easy/661.Image Smoother.py
@@ -21,3 +21,37 @@ Note:
 """
 
 def imageSmoother(M: List[List[int]]) -> List[List[int]]:
+    # 260 ms, faster than 100%. The best solution from submissions is 296 ms.
+    R, C = len(M), len(M[0])
+    # special cases:
+    if R < 3 and C < 3:
+        s = sum([sum(L) for L in M])
+        return [[s // (R * C)] * C for _ in M]
+    if R == 1:
+        return [[(M[0][0] + M[0][1]) // 2] + [(M[0][i] + M[0][i + 1] + M[0][i + 2]) // 3 for i in range(C - 2)] + [(M[0][-2] + M[0][-1]) // 2]]
+    if C == 1:
+        return [[(M[0][0] + M[1][0]) // 2]] + [[(M[i][0] + M[i + 1][0] + M[i + 2][0]) // 3] for i in range(R - 2)] + [[(M[-2][0] + M[-1][0]) // 2]]
+
+    # common cases:
+    Rl, Cl = R - 1, C - 1
+    ans = [[0] * C for _ in M]
+
+    ass = [[0] * (C) for _ in M]
+    for r in range(R):
+        ass[r][0] = M[r][0] + M[r][1]
+        ass[r][Cl] = M[r][Cl] + M[r][Cl - 1]
+        for c in range(1, Cl):
+            ass[r][c] = M[r][c - 1] + M[r][c] + M[r][c + 1]
+
+    for c in [0, Cl]:
+        ans[0][c] = (ass[0][c] + ass[1][c]) // 4
+        ans[Rl][c] = (ass[Rl][c] + ass[Rl - 1][c]) // 4
+        for r in range(1, Rl):
+            ans[r][c] = (ass[r - 1][c] + ass[r][c] + ass[r + 1][c]) // 6
+    for c in range(1, Cl):
+        ans[0][c] = (ass[0][c] + ass[1][c]) // 6
+        ans[Rl][c] = (ass[Rl][c] + ass[Rl - 1][c]) // 6
+        for r in range(1, Rl):
+            ans[r][c] = (ass[r - 1][c] + ass[r][c] + ass[r + 1][c]) // 9
+
+    return ans
